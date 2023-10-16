9c98892b58916ff07b38e19dc69dffb5895ae913
Jishnu-Dev <71690306+Jishnu-Dev@users.noreply.github.com>
2023-10-17 03:23:23 +0530

    Docker configuration

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server'
 import { type NextRequest } from 'next/server'
 import { Pool } from 'pg'
 
-const pool = new Pool({
+export const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: 'db',
   database: 'luxo_task_db',
   password: 'docker',
   port: 5432
@@ -18,25 +18,40 @@ export async function GET(request: NextRequest) {
 
     if (page) {
       const offset = (parseInt(page) - 1) * limit
+
+      console.error('Connecting to the database......')
       const client = await pool.connect()
+      console.error('Connected to the database.')
+
+      console.error('Executing query...')
       try {
         const res = await client.query(`SELECT * FROM ads OFFSET $1 LIMIT $2`, [
           offset,
           limit
         ])
+        console.info('Query executed.')
 
         const rows = res.rows
         const totalRes = await client.query(`SELECT COUNT(*) FROM ads`)
         const totalCount = parseInt(totalRes.rows[0].count, 10)
         const totalPages = Math.ceil(totalCount / limit)
 
+        console.info('Completed.')
         return NextResponse.json({ rows, totalPages })
+      } catch (e) {
+        console.error('Eeeeee', e)
       } finally {
         client.release()
       }
     }
   } catch (e) {
     console.error(e)
-    return NextResponse.json({ error: 'An error occurred' })
+    if (e instanceof Error) {
+      console.error(e.message)
+      return NextResponse.json({ error: e.message })
+    } else {
+      console.error(e)
+      return NextResponse.json({ error: 'An unexpected error occurred' })
+    }
   }
 }
--- populate-db.ts
+++ populate-db.ts
@@ -1,34 +1,84 @@
 const { Pool } = require('pg')
 const fs = require('fs')
 
-const dbConfig = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+console.info('Info:: ENTERED SCRIPT...')
+export const pool = new Pool({
+  user: 'postgres',
+  host: 'db',
+  database: 'luxo_task_db',
+  password: 'docker',
+  port: 5432
+})
+
+// async function createDatabase() {
+//   const client = await pool.connect()
+//   try {
+//     await client.query(
+//       `CREATE DATABASE IF NOT EXISTS ${process.env.NEXT_PUBLIC_DB_NAME}`
+//     )
+//   } finally {
+//     client.release()
+//   }
+// }
+
+async function createTables() {
+  console.info('Info:: CONNECTING TO DB...')
+  const client = await pool.connect()
+  console.info('Info:: CREATING TABLE...')
+  try {
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS ads (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255),
+        location VARCHAR(255),
+        image_url TEXT
+      )
+    `)
+  } catch (e) {
+    console.error(e)
+  } finally {
+    client.release()
+  }
 }
 
-const pool = new Pool(dbConfig)
-const jsonFilePath = './public/ads.json'
-const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'))
-
-const insertQuery = `INSERT INTO ads (title, location, image_url) VALUES ($1, $2, $3)`
-;(async () => {
-  console.info('POPULATION STARTED...')
+async function populateAdsTable() {
   const client = await pool.connect()
+  console.info('Info:: POPULATING TABLE...')
+
   try {
     await client.query('BEGIN')
+    const jsonFilePath = './public/ads.json'
+    const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'))
+
     for (const data of jsonData) {
-      const values = [data.title, data.location, data.image]
-      await client.query(insertQuery, values)
+      const values = [data.title, data.location, data.image_url]
+      await client.query(
+        'INSERT INTO ads (title, location, image_url) VALUES ($1, $2, $3)',
+        values
+      )
     }
     await client.query('COMMIT')
   } catch (error) {
     await client.query('ROLLBACK')
     throw error
   } finally {
-    console.info('POPULATION COMPLETED...')
     client.release()
   }
-})()
+}
+
+async function main() {
+  try {
+    // await createDatabase()
+    await createTables()
+    await populateAdsTable()
+    console.info(
+      'Database and tables created, and data populated successfully.'
+    )
+  } catch (error) {
+    console.error('Error:', error)
+  } finally {
+    await pool.end()
+  }
+}
+
+main()
