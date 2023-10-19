9033c1b246926f12a575f2dd6023b8eebfa1c436
Jishnu-Dev <71690306+Jishnu-Dev@users.noreply.github.com>
2023-10-19 17:37:41 +0530

    Added framer motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from '@/components/layout/Footer'
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 import NavBar from '@/components/layout/NavBar'
+import ScrollProgress from '@/components/layout/ScrollProgress'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ScrollProgress />
         <NavBar />
         <main>{children}</main>
         <Footer />
